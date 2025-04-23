5ea753f10a37106eeeef58b9164a14d906fe21f3
王先生 <1324771795@qq.com>
2025-04-23 20:49:51 +0800

    添加项目文件。

---
+++ Slangc.NET/SlangStage.cs
@@ -0,0 +1,36 @@
+﻿namespace Slangc.NET;
+
+public enum SlangStage : uint
+{
+    None,
+
+    Vertex,
+
+    Hull,
+
+    Domain,
+
+    Geometry,
+
+    Pixel,
+
+    Compute,
+
+    RayGeneration,
+
+    Intersection,
+
+    AnyHit,
+
+    ClosestHit,
+
+    Miss,
+
+    Callable,
+
+    Mesh,
+
+    Amplification,
+
+    Count
+};
