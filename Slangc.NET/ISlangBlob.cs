0305eac49143387a2980fbc017e37de5d0fe2268
王先生 <1324771795@qq.com>
2025-04-23 20:49:51 +0800

    添加项目文件。

---
+++ Slangc.NET/ISlangBlob.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Slangc.NET;
+
+public unsafe struct ISlangBlob
+{
+    public void** LpVtbl;
+
+    public readonly void* GetBufferPointer()
+    {
+        ISlangBlob* @this = (ISlangBlob*)Unsafe.AsPointer(ref Unsafe.AsRef(in this));
+
+        return ((delegate* unmanaged[Stdcall]<ISlangBlob*, void*>)@this->LpVtbl[3])(@this);
+    }
+
+    public readonly ulong GetBufferSize()
+    {
+        ISlangBlob* @this = (ISlangBlob*)Unsafe.AsPointer(ref Unsafe.AsRef(in this));
+
+        return ((delegate* unmanaged[Stdcall]<ISlangBlob*, ulong>)@this->LpVtbl[4])(@this);
+    }
+};
---
+++ Slangc.NET/SlangResult.cs
@@ -0,0 +1,13 @@
+﻿namespace Slangc.NET;
+
+public readonly struct SlangResult(int code)
+{
+    public bool IsOk => code is 0;
+
+    public bool IsError => code is not 0;
+
+    public static implicit operator SlangResult(int code)
+    {
+        return new(code);
+    }
+}
