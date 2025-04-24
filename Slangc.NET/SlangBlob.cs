8b1a1afb0962a77e0ebc8b81dd3ef28392cfc290
王先生 <1324771795@qq.com>
2025-04-24 11:47:53 +0800

    重命名 ISlangBlob 为 SlangBlob
    在 `ISlangBlob.cs` 文件中，将 `ISlangBlob` 结构体替换为 `SlangBlob` 结构体，保持成员和方法不变，并添加了命名空间和使用指令。在 `SlangCompileRequest.cs` 文件中，更新了 `spReflection_ToJson` 方法的参数类型和 `GetReflectionJson` 方法中的 `outBlob` 类型，以确保与新的 `SlangBlob` 结构体一致。

---
+++ Slangc.NET/SlangBlob.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Slangc.NET;
+
+public unsafe struct SlangBlob
+{
+    public void** LpVtbl;
+
+    public readonly void* GetBufferPointer()
+    {
+        SlangBlob* @this = (SlangBlob*)Unsafe.AsPointer(ref Unsafe.AsRef(in this));
+
+        return ((delegate* unmanaged[Stdcall]<SlangBlob*, void*>)@this->LpVtbl[3])(@this);
+    }
+
+    public readonly ulong GetBufferSize()
+    {
+        SlangBlob* @this = (SlangBlob*)Unsafe.AsPointer(ref Unsafe.AsRef(in this));
+
+        return ((delegate* unmanaged[Stdcall]<SlangBlob*, ulong>)@this->LpVtbl[4])(@this);
+    }
+};
