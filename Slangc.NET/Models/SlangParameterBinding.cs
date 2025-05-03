16ef8d5bba39b927bb3e974e1bfd9a27038062d4
王先生 <1324771795@qq.com>
2025-05-03 23:00:06 +0800

    支持 Slangc.NET 并更新着色器编译配置
    在 `Program.cs` 中添加对 `Slangc.NET` 的支持，更新编译参数以使用新的着色器文件，移除对 `spirv` 的目标设置，改为使用 `sm_6_6` 配置。
    
    在 `SlangEntryPoint.cs` 中更新 `SlangEntryPoint` 类，新增 `Bindings` 属性以存储参数绑定信息。
    
    在 `SlangParameterBinding.cs` 中新增 `SlangParameterBinding` 类，包含从 JSON 对象反序列化参数绑定的逻辑，定义 `Name` 和 `Binding` 属性。

---
+++ Slangc.NET/Models/SlangParameterBinding.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Slangc.NET.Models;
+
+public class SlangParameterBinding
+{
+    internal SlangParameterBinding(JsonObject reader)
+    {
+        Name = reader["name"].Deserialize<string>();
+        Binding = new(reader["binding"]!.AsObject());
+    }
+
+    public string Name { get; }
+
+    public SlangBinding Binding { get; }
+}
