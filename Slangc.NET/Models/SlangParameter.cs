43e6ff06d47d3dbf37634f4569df96ef5ccf037a
王先生 <1324771795@qq.com>
2025-05-03 14:57:49 +0800

    添加枚举和类以支持参数和类型分类
    在 `SlangParameterCategory.cs` 中添加 `SlangParameterCategory` 枚举，定义多种参数类别。
    在 `SlangTypeKind.cs` 中添加 `SlangTypeKind` 枚举，定义多种类型。
    在 `SlangBinding.cs` 中添加 `SlangBinding` 类，包含参数类别、空间、索引和使用状态。
    在 `SlangParameter.cs` 中添加 `SlangParameter` 类，包含名称和绑定属性。
    在 `SlangType.cs` 中添加 `SlangType` 类，包含类型属性 `Kind`。

---
+++ Slangc.NET/Models/SlangParameter.cs
@@ -0,0 +1,8 @@
+﻿namespace Slangc.NET.Models;
+
+public class SlangParameter(string name, SlangBinding binding)
+{
+    public string Name { get; } = name;
+
+    public SlangBinding Binding { get; } = binding;
+}
