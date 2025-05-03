e00f56791b857b22cb395c2cc5f8c3a84cc531c2
王先生 <1324771795@qq.com>
2025-05-03 19:04:22 +0800

    更新类型枚举和参数类以支持新结构
    - 修改 `SlangParameterCategory` 枚举，移除 `Unknown` 项，添加 `CountV1` 项。
    - 更新 `Parameter` 类构造函数，新增 `Type` 属性，类型为 `TypeLayoutInfo`。
    - 新增 `SlangTypeKind` 枚举，定义多种类型。
    - 新增 `TypeInfo` 类，支持从 JSON 反序列化 `Kind` 属性。
    - 新增 `TypeLayoutInfo` 类，继承自 `TypeInfo`，实现构造函数。

---
+++ Slangc.NET/Models/TypeInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Nodes;
+using Slangc.NET.Enums;
+
+namespace Slangc.NET.Models;
+
+/// <summary>
+/// slang-reflection-json.cpp
+/// static void emitReflectionTypeInfoJSON(PrettyWriter& writer, slang::TypeReflection* type)
+/// </summary>
+public class TypeInfo
+{
+    internal TypeInfo(JsonObject reader)
+    {
+        Kind = reader["kind"].Deserialize<SlangTypeKind>();
+    }
+
+    public SlangTypeKind Kind { get; }
+}
