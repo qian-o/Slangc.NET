7167cbf55c9ba639cc900c02e3d17ddbf991b203
qian-o <1324771795@qq.com>
2025-06-17 17:24:59 +0800

    Update SlangEntryPoint constructor initialization
    Modified the `SlangEntryPoint` constructor to properly initialize the `ThreadGroupSize` property using a conditional expression. The initialization for the `Bindings` property remains unchanged.

--- Slangc.NET/Models/SlangEntryPoint.cs
+++ Slangc.NET/Models/SlangEntryPoint.cs
@@ -9,15 +9,15 @@ public class SlangEntryPoint
     {
         Name = reader["name"].Deserialize<string>();
         Stage = reader["stage"].Deserialize<SlangStage>();
-        Bindings = [.. reader["bindings"]!.AsArray().Select(static reader => new SlangNamedTypeBinding(reader!.AsObject()))];
         ThreadGroupSize = reader.ContainsKey("threadGroupSize") ? [.. reader["threadGroupSize"]!.AsArray().Select(static reader => reader!.GetValue<uint>())] : [];
+        Bindings = [.. reader["bindings"]!.AsArray().Select(static reader => new SlangNamedTypeBinding(reader!.AsObject()))];
     }
 
     public string Name { get; }
 
     public SlangStage Stage { get; }
 
-    public SlangNamedTypeBinding[] Bindings { get; set; }
-
     public uint[] ThreadGroupSize { get; set; }
+
+    public SlangNamedTypeBinding[] Bindings { get; set; }
 }
